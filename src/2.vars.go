ebc24cad45ee58372a39fdacf8868bffcc4a93c4
Azhar <azhar.khan@peak.ai>
2022-12-07 18:20:40 +0530

    feat: training phase 1

---
+++ src/2.vars.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = 1
+	var b int
+	var c = "string"
+	d := "anoother-string"
+	e, f := "hello", 5
+	var g, h int = 7, 6
+	fmt.Println(a, b, c, d, e, f, g, h)
+}
